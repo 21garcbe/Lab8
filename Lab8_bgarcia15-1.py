7b47189fafc4bb15c5bb60fa31d3710d50cda347
21garcbe <21garcbe@gmail.com>
2026-03-06 11:49:10 -0500

    removed unnecessary return in find_UPC and made comments less verbose/moved algo explanation to function docstring

--- Lab8_bgarcia15-1.py
+++ Lab8_bgarcia15-1.py
@@ -1,30 +1,40 @@
 """
 Lab 8: UPC Validator
 Author: Ben Garcia
-Program that validates user provided 12 digit UPC-A codes,
+Program that validates user provided 12 digit UPC-A codes
 
 """
 def find_UPC(upc):
     """Checks if the provided UPC code is valid or not.
     Args:
-        upc: A string representing a UPC code."""
+        upc: A string representing a UPC code.    
+
+        Algorithm steps:
+        1. Sum odd and even digits seperately using range function to iterate over UPC integer
+        assign sums seperately to sum_odd, sum_even variables
+        2. Multiply sum_odd by 3
+        3. Add sum_odd and sum_even together to get total_sum
+        4. Calculate check digit by taking total_sum mod 10, 
+        if the result is 0 then check digit is 0, otherwise it is 10 - (total_sum mod 10)
+        5. Compare the calculated check digit to the last digit of the UPC code (cast to an integer) 
+        and print if the UPC code is valid or not.
+        """
     print(f"The first 11 digits are: {upc[:-1]}.")
     print(f"\nThe provided check digit is {upc[-1]}")
     print("\nCalculating check digit...")
-    #initialize odd and even sums for algorithm
     sum_odd = 0
     sum_even = 0
-    #loop through odd sums and multiply by 3, using range function to skip even numbers
+    
     for i in range(0, 11, 2):
         sum_odd += int(upc[i])
     sum_odd *= 3
-    #loop through even sums, using range function to skip odd numbers
+    
     for i in range(1, 11, 2):
         sum_even += int(upc[i])
 
     total_sum = sum_odd + sum_even
 
-    #calculate check digit (= total sum mod 10, if 0 check digit is 0, otherwise it is 10 - mod 10)
+    #calculate check digit 
     modulo = total_sum % 10
     if modulo == 0:
         check_digit = 0
@@ -32,13 +42,12 @@ def find_UPC(upc):
         check_digit = 10 - modulo 
     print(f"\nThe calculated check digit is {check_digit}.")
     
-    #cast the last digit of the UPC code to an integer and compare it to the calculated check digit
+    #compare provided check digit to the calculated check digit
     if check_digit == int(upc[-1]):
         print("The UPC code is valid.")
     else:
         print("The UPC code is invalid.")
 
-    return check_digit
 
 
 #Main program
